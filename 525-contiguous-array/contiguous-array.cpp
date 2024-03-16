01e1d8b996d4c9a40881568254983639d011dced
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-03-16 17:54:24 +0700

    Time: 84 ms (66.28%) | Memory: 87.8 MB (27.04%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        //if (nums.size() == 2) return 2;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int prefixSum = 0;
+        int length = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            prefixSum += (nums[i] == 1) ? 1 : -1;
+
+            if (mp.find(prefixSum) != mp.end())
+            {
+                length = max(length, i - mp[prefixSum]);
+            }
+            else mp[prefixSum] = i;
+        }
+        return length;
+    }
+};
