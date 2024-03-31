74687018f6216c1ada65db8148cf3bb50b09f8bd
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-03-31 20:50:58 +0700

    Time: 619 ms (75.61%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -2,5 +2,6 @@
 select query_name, round(avg(cast(rating as decimal)/position), 2) as quality,
 round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
 from Queries
-where query_name is not null
+-- where query_name is not null
 group by query_name
+having query_name is not null
