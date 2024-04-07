cb0ea47576a6fdcd89374ccdfa4e0ff16bc092d7
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-04-07 07:24:41 +0700

    Time: 3 ms (45.50%) | Memory: 7.4 MB (53.16%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0;
+        int leftMax = 0;
+
+        for (auto c : s)
+        {
+            if (c == '(')
+            {
+                leftMin++;
+                leftMax++;
+            }
+            else if (c == ')')
+            {
+                leftMin--;
+                leftMax--;
+            }
+            else
+            {
+                leftMin--;
+                leftMax++;
+            }
+            
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+
+        return leftMin == 0;
+    }
+};
