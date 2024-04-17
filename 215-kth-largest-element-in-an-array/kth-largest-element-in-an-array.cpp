2e75c09a1d227157071c87b279e4f7cb1aaa7d4d
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-04-18 00:02:04 +0700

    Time: 97 ms (53.20%) | Memory: 64.3 MB (19.18%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        int pivot = nums[rand() % n];
-        vector<int> left, right, mid;
-        for (auto num : nums)
-        {
-            if (num > pivot) 
-                right.push_back(num);
-            else if (num < pivot)
-                left.push_back(num);
-            else
-                mid.push_back(num);
-        }
-        if (k <= right.size())
-            return findKthLargest(right, k);
+        // int n = nums.size();
+        // int pivot = nums[rand() % n];
+        // vector<int> left, right, mid;
+        // for (auto num : nums)
+        // {
+        //     if (num > pivot) 
+        //         right.push_back(num);
+        //     else if (num < pivot)
+        //         left.push_back(num);
+        //     else
+        //         mid.push_back(num);
+        // }
+        // if (k <= right.size())
+        //     return findKthLargest(right, k);
         
-        k -= right.size();
+        // k -= right.size();
 
-        if (k <= mid.size())
-            return pivot;
+        // if (k <= mid.size())
+        //     return pivot;
 
-        k -= mid.size();
+        // k -= mid.size();
 
-        return findKthLargest(left, k);
+        // return findKthLargest(left, k);
+
+        priority_queue<int> pq;
+        int n = nums.size();
+        for (int i = 0; i < n; i ++)
+            pq.push(nums[i]);
+
+        int ans = 0;
+        for (int i = 0; i < k; i++)
+        {
+            ans = pq.top();
+            pq.pop();
+        }
+        
+        return ans;
     }
 };
