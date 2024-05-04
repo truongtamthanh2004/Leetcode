7b4791370b6f74977e4849202f9cae973ac9a5bc
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-05-04 11:30:13 +0700

    Time: 67 ms (72.01%) | Memory: 54.6 MB (63.32%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int ans = 0;
+        int maxLength = 0;
         int n = nums.size();
         int j = 0;
         deque<int> dqmin, dqmax;
 
         for (int i = 0; i < n; i++)
         {
-            while (dqmax.size() > 0 && dqmax.back() < nums[i]) dqmax.pop_back();
+            while (!dqmax.empty() && dqmax.back() < nums[i]) dqmax.pop_back();
 
-            while (dqmin.size() > 0 && dqmin.back() > nums[i]) dqmin.pop_back();
+            while (!dqmin.empty() && dqmin.back() > nums[i]) dqmin.pop_back();
 
             dqmin.push_back(nums[i]);
             dqmax.push_back(nums[i]);
@@ -18,15 +18,15 @@ public:
             int maxE = dqmax.front();
             int minE = dqmin.front();
 
-            if (maxE - minE <= limit) ans = max(i - j + 1, ans);
+            if (maxE - minE <= limit) maxLength = max(maxLength, i - j + 1);
             else 
             {
                 if (nums[j] == dqmax.front()) dqmax.pop_front();
-                if (nums[j] == dqmin.front()) dqmin.pop_front();
+                else if (nums[j] == dqmin.front()) dqmin.pop_front();
                 j++;
             }
         }
 
-        return ans;
+        return maxLength;
     }
 };
