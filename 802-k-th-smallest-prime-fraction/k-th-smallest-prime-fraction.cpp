81cf38b61921ca2d388e782eb403d44b9083f2ec
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-05-10 07:38:07 +0700

    Time: 375 ms (36.75%) | Memory: 66.7 MB (40.81%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        vector<pair<int, int>> fraction;
+        for(int i = 0; i < arr.size() - 1; i++)
+        {
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                fraction.push_back({arr[i], arr[j]});
+            }
+        }
+
+        std::sort(fraction.begin(), fraction.end(), [](pair<int, int>& a, pair<int, int>& b) {
+            return (double)a.first / a.second < (double)b.first / b.second;  // Return true if `a` should come before `b`
+        });
+
+        vector<int> ans;
+        ans.push_back(fraction[k - 1].first);
+        ans.push_back(fraction[k - 1].second);
+        return ans;
+    }
+};
