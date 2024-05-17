6d3ea7e5086f1456a4eeef793f8f17f721a44a10
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-05-17 11:34:26 +0700

    Time: 11 ms (64.37%) | Memory: 21.3 MB (47.43%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -13,8 +13,10 @@ class Solution {
 public:
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
         if (!root) return nullptr;
-        root->left = removeLeafNodes(root->left, target);
-        root->right = removeLeafNodes(root->right, target);
+        if (root->left) 
+            root->left = removeLeafNodes(root->left, target);
+        if (root->right) 
+            root->right = removeLeafNodes(root->right, target);
 
         if (!root->left && !root->right && root->val == target) return nullptr;
 
