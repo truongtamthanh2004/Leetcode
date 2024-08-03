fdc97ada4a1358e0345175a7bba8f9848db51999
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-08-03 09:20:04 +0700

    Time: 14 ms (24.92%) | Memory: 17.1 MB (88.10%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -3,6 +3,8 @@ public:
     bool checkAllElementsIn(vector<int>& target, vector<int>& arr) {
         for (auto i : target) {
             int cnt = count(arr.begin(), arr.end(), i);
+            if (cnt == 0) return false;
+
             int cnt2 = count(target.begin(), target.end(), i);
 
             if (cnt != cnt2) return false;
