bddab67f0c3ddc837eaa1f836374e28e9045b7da
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-08-03 09:26:32 +0700

    Time: 8 ms (66.96%) | Memory: 17.8 MB (33.93%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // 1st solution
     bool checkAllElementsIn(vector<int>& target, vector<int>& arr) {
         for (auto i : target) {
             int cnt = count(arr.begin(), arr.end(), i);
@@ -13,7 +14,26 @@ public:
         return true;
     }
 
+    // 2nd solution
+    bool checkAllElementsIn2(vector<int>& target, vector<int>& arr) {
+        vector<int> freq(1001, 0);
+        int m = 0;
+
+        for (int i = 0; i < target.size(); i++) {
+            freq[target[i]]++;
+            freq[arr[i]]--;
+            m = max(m, max(target[i], arr[i]));
+        }
+
+        for (int i = 1; i <= m; i++) {
+            if (freq[i] != 0) return false;
+        }
+
+        return true;
+    }
+
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        return checkAllElementsIn(target, arr);
+        // return checkAllElementsIn(target, arr);
+        return checkAllElementsIn2(target, arr);
     }
 };
