3e6824bfefc1adeada060e4a85b5315d59957d7b
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-08-06 11:05:22 +0700

    Time: 78 ms (41.29%) | Memory: 26.1 MB (49.94%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    static bool compare(pair<int, char>& a, pair<int, char>& b) { return a.first > b.first; }
+    static bool compare(int& a, int& b) { return a > b; }
 
     int minimumPushes(string word) {
-        unordered_map<char, int> mp;
-        for (auto i : word) mp[i]++;
+        vector<int> mp(26, 0);
+        for (auto i : word) mp[i - 'a']++;
 
-        vector<pair<int, char>> arr;
-        for (auto i : mp) {
-            arr.push_back({i.second, i.first});
-        }
+        sort(mp.begin(), mp.end(), compare);
 
-        sort(arr.begin(), arr.end(), compare);
-
-        int count = 0;
-        int push = 1;
         int ans = 0;
-        for (auto i : arr) {
-            ans += (i.first * push);
-            count++;
-            if (count % 8 == 0) push++;
+        for (int i = 0; i < 26; i++) {
+            ans += mp[i] * (i / 8 + 1);
         }
 
         return ans;
