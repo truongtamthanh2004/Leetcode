d8384dee6e5f6188a905563316ab7954b8b3100f
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-08-09 08:26:19 +0700

    Time: 5 ms (32.35%) | Memory: 12.2 MB (18.49%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,160 @@
+// class Solution {
+// public:
+//     int calcSumRow(vector<vector<int>>& grid, int r, int c) {
+        
+//         int sumEqual = 0;
+//         for (int i = r; i <= r + 2; i++) {
+//             int sum = 0;
+//             for (int j = c; j <= c + 2; j++) {
+//                 sum += grid[i][j];
+//             }
+//             if (i == r) {
+//                 sumEqual = sum;
+//             }
+//             else {
+//                 if (sumEqual != sum) return 0;
+//             }
+//         }
+//         return sumEqual;
+//     }
+//     int calcSumCol(vector<vector<int>>& grid, int r, int c) {
+        
+//         int sumEqual = 0;
+//         for (int i = c; i <= c + 2; i++) {
+//             int sum = 0;
+//             for (int j = r; j <= r + 2; j++) {
+//                 sum += grid[i][j];
+//             }
+//             if (i == c) {
+//                 sumEqual = sum;
+//             }
+//             else {
+//                 if (sumEqual != sum) return 0;
+//             }
+//         }
+//         return sumEqual;
+//     }
+//     int calcSumDiaMain(vector<vector<int>>& grid, int r, int c) {
+//         int sum = 0;
+//         int i = r, j = c;
+//         while (i <= r + 2) {
+//             sum += grid[i][j];
+//             i++;
+//             j++;
+//         }
+//         return sum;
+//     }
+
+//     int calcSumDiaRemain(vector<vector<int>>& grid, int r, int c) {
+//         int sum = 0;
+//         int i = r, j = c;
+//         while (i >= r - 2) {
+//             sum += grid[i][j];
+//             i--;
+//             j--;
+//         }
+//         return sum;
+//     }
+
+//     bool isDistinct(vector<vector<int>>& grid, int r, int c) {
+//         unordered_map<int, int> mp;
+//         for (int i = r; i <= r + 2; i++) {
+//             for (int j = c; j <= c + 2; j++) {
+//                 mp[grid[i][j]]++;
+//                 if (mp[grid[i][j]] > 1) return false; 
+//             }
+//         }
+
+//         return true;
+//     }
+//     int numMagicSquaresInside(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+
+//         int cnt = 0;
+//         for (int i = 0; i < m - 2; i++) {
+//             for (int j = 0; j < n - 2; j++) {
+//                 if (isDistinct(grid, i, j) && calcSumRow(grid, i, j) > 0 
+//                     && calcSumRow(grid, i, j) == calcSumCol(grid, i, j) && calcSumDiaMain(grid, i, j) == calcSumDiaRemain(grid, i + 2, j + 2)) cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+class Solution {
+public:
+    // Function to calculate the sum of a specific row
+    int calcSumRow(const vector<vector<int>>& grid, int r, int c) {
+        int sum = 0;
+        for (int j = c; j < c + 3; ++j) {
+            sum += grid[r][j];
+        }
+        return sum;
+    }
+
+    // Function to calculate the sum of a specific column
+    int calcSumCol(const vector<vector<int>>& grid, int r, int c) {
+        int sum = 0;
+        for (int i = r; i < r + 3; ++i) {
+            sum += grid[i][c];
+        }
+        return sum;
+    }
+
+    // Function to calculate the sum of the main diagonal
+    int calcSumDiaMain(const vector<vector<int>>& grid, int r, int c) {
+        int sum = 0;
+        for (int i = 0; i < 3; ++i) {
+            sum += grid[r + i][c + i];
+        }
+        return sum;
+    }
+
+    // Function to calculate the sum of the other diagonal
+    int calcSumDiaRemain(const vector<vector<int>>& grid, int r, int c) {
+        int sum = 0;
+        for (int i = 0; i < 3; ++i) {
+            sum += grid[r + 2 - i][c + i];
+        }
+        return sum;
+    }
+
+    // Function to check if all numbers in the 3x3 sub-grid are distinct and between 1 and 9
+    bool isDistinct(const vector<vector<int>>& grid, int r, int c) {
+        unordered_set<int> seen;
+        for (int i = r; i < r + 3; ++i) {
+            for (int j = c; j < c + 3; ++j) {
+                if (grid[i][j] < 1 || grid[i][j] > 9 || !seen.insert(grid[i][j]).second) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Function to count the number of magic squares in the grid
+    int numMagicSquaresInside(const vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+        
+        for (int i = 0; i <= m - 3; ++i) {
+            for (int j = 0; j <= n - 3; ++j) {
+                if (isDistinct(grid, i, j)) {
+                    int rowSum = calcSumRow(grid, i, j);
+                    int colSum = calcSumCol(grid, i, j);
+                    int diaMain = calcSumDiaMain(grid, i, j);
+                    int diaRemain = calcSumDiaRemain(grid, i, j);
+                    
+                    // Check if all sums are equal to the magic sum (15)
+                    if (rowSum == 15 && colSum == 15 && diaMain == 15 && diaRemain == 15) {
+                        ++count;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
