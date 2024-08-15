fd2ff2c60eb21f793f8b3f2deac39bea40f4a3be
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2024-08-15 07:43:24 +0700

    Time: 127 ms (5.56%) | Memory: 86 MB (50.86%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        vector<int> cntMoney(2, 0);
+
+        for (auto i : bills) {
+            if (i == 20) {
+                int remaining = 15;
+                if (cntMoney[1] > 0) {
+                    cntMoney[1]--;
+                    remaining -= 10;
+                }
+                    
+                while (remaining >= 5) {
+                    if (cntMoney[0] == 0) return false;
+                    cntMoney[0]--;
+                    remaining -= 5;
+                }
+            }
+            if (i == 10) {
+                cntMoney[1]++;
+                if (cntMoney[0] == 0) return false;
+                cntMoney[0]--;
+            }
+            if (i == 5) cntMoney[0]++;
+            cout << cntMoney[0] << " " << cntMoney[1] << endl;
+        }
+        return true;
+    }
+};
