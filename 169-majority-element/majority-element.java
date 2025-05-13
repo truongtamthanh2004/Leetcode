b259c5ef4be02a883c67c5d0e2951f82958a02a4
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2025-05-13 22:25:48 +0700

    Time: 15 ms (22.07%) | Memory: 49.5 MB (86.26%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (!map.containsKey(i)) {
+                map.put(i, 1);
+                continue;
+            }
+            map.put(i, map.get(i) + 1);
+        }
 
-        return nums[nums.length / 2];
+        int maxKey = 0;
+        int maxVal = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer val = entry.getValue();
+            
+            if (maxVal < val) {
+                maxVal = val;
+                maxKey = key;
+            }
+        }
+
+        return maxKey;
     }
 }
